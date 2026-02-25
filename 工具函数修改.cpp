6312a35ec2e910820f03f770d20d9ed844691cfc
xyj-abc <18656050163@163.com>
2026-02-25 19:37:55 +0800

    添加了工具函数进行判断是否有重复学号

---
+++ 工具函数修改.cpp
@@ -0,0 +1,42 @@
+Student* createStudent(int id, const char* name, int age, float score) {
+    Student* s = (Student*)malloc(sizeof(Student));
+    s->id = id;
+    strncpy(s->name, name, MAX_NAME_LEN - 1);
+    s->name[MAX_NAME_LEN - 1] = '\0';
+    s->age = age;
+    s->score = score;
+    s->next = NULL;
+    s->prev = NULL;
+    return s;
+}
+
+void appendStudent(Student** head, Student* newNode) {
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    Student* temp = *head;
+    while (temp->next) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
+void freeStudents(Student* head) {
+    Student* temp;
+    while (head) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+Student* findStudentById(int id) {
+    Student* temp = studentList;
+    while (temp) {
+        if (temp->id == id)
+            return temp;
+        temp = temp->next;
+    }
+    return NULL;
+}
